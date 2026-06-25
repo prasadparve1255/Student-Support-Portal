a5f421b270c43dd27b58298ff754647b92a1e41b
Prasad Parve <prasadpandurangparve@gmail.com>
2026-06-26 01:18:01 +0530

    Added nodemailer dependency

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,5 +1,7 @@
 const nodemailer = require('nodemailer');
+const dns = require("dns");
 
+dns.setDefaultResultOrder("ipv4first");
 // const transporter = nodemailer.createTransport({
 //   service: 'gmail',
 //   auth: {
@@ -7,6 +9,10 @@ const nodemailer = require('nodemailer');
 //     pass: process.env.EMAIL_PASSWORD,
 //   },
 // });
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
