ef559555fa878555283d15bed9bf345d318618a5
Prasad Parve <prasadpandurangparve@gmail.com>
2026-06-26 01:04:14 +0530

    Added nodemailer dependency

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,12 +1,31 @@
 const nodemailer = require('nodemailer');
 
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASSWORD,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
+
+console.log("EMAIL_USER =", process.env.EMAIL_USER);
+console.log(
+  "EMAIL_PASSWORD EXISTS =",
+  !!process.env.EMAIL_PASSWORD
+);
 
 transporter.verify((error, success) => {
   if (error) {
